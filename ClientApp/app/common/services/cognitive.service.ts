c86a5f14c04bd155a1280a3d48a52e0bbc54fd5e
André Minelli <andreminelli@gmail.com>
2018-03-10 14:13:39 -0300

    Search component implementation

--- ClientApp/app/common/services/cognitive.service.ts
+++ ClientApp/app/common/services/cognitive.service.ts
@@ -7,10 +7,10 @@ import { AzureHttpClient } from './azureHttpClient';
 import { BingSearchResponse } from '../models/bingSearchResponse';
 @Injectable()
 export class CognitiveService {
-    bingSearchAPIKey = '';
+    bingSearchAPIKey = '58ef7674237a4b929571f7636db6f5c0';
     constructor(private http: AzureHttpClient) { }
     searchImages(searchTerm: string): Observable<BingSearchResponse> {
-        return this.http.get(`https://api.cognitive.microsoft.com/bing/v5.0/images/search?q=${searchTerm}`, this.bingSearchAPIKey)
+        return this.http.get(`https://api.cognitive.microsoft.com/bing/v7.0/images/search?q=${searchTerm}`, this.bingSearchAPIKey)
             .map(response => response.json() as BingSearchResponse)
             .catch(this.handleError);
     }
